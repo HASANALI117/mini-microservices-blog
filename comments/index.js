070f8e7b2bfdcaf9ef78b05af4a569f7ea8bb377
HASANALI117 <hassanali61911@gmail.com>
2024-07-12 11:30:55 +0300

    Add events to Comment service

--- comments/index.js
+++ comments/index.js
@@ -3,6 +3,7 @@ const bodyParser = require("body-parser");
 const { randomBytes } = require("crypto");
 const cors = require("cors");
 const axios = require("axios");
+const { type } = require("os");
 
 const app = express();
 app.use(bodyParser.json());
@@ -14,7 +15,7 @@ app.get("/posts/:id/comments", (req, res) => {
   res.send(commentsByPostId[req.params.id] || []);
 });
 
-app.post("/posts/:id/comments", (req, res) => {
+app.post("/posts/:id/comments", async (req, res) => {
   const commentId = randomBytes(4).toString("hex");
   const { content } = req.body;
 
@@ -23,6 +24,11 @@ app.post("/posts/:id/comments", (req, res) => {
   comments.push({ id: commentId, content });
 
   commentsByPostId[req.params.id] = comments;
+
+  await axios.post("http://localhost:4005/events", {
+    type: "CommentCreated",
+    data: { id: commentId, content, postId: req.params.id },
+  });
 
   res.status(201).send(comments);
 });
